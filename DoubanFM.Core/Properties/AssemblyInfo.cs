7d74a1330b3d401a02b7ef54f02990fc374ee88c
Kai Yang <kfstorm@users.noreply.github.com>
2011-08-10 12:32:24 +0000

    发布1.2.0 Beta2

--- DoubanFM.Core/Properties/AssemblyInfo.cs
+++ DoubanFM.Core/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
- [assembly: AssemblyVersion("1.2.0.5147")]
+ [assembly: AssemblyVersion("1.2.0.5199")]
 //[assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
