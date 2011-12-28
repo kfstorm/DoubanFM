5deee1c6ae241ee3cce96c9b927a1721afd74b46
Kai Yang <kfstorm@users.noreply.github.com>
2011-12-28 11:26:28 +0000

    添加LICENSE，补上遗漏的文件

---
+++ DoubanFM/NotifyIcon/PopupControlPanel.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DoubanFM.Core;
+using System.Windows.Media.Animation;
+
+namespace DoubanFM.NotifyIcon
+{
+	/// <summary>
+	/// NotifyIconPopup.xaml 的交互逻辑
+	/// </summary>
+	public partial class PopupControlPanel : UserControl
+	{
+		public PopupControlPanel()
+		{
+			InitializeComponent();
+
+			_player = FindResource("Player") as Player;
+		}
+
+		public void ShowCoverSmooth()
+		{
+			((Storyboard)FindResource("ShowCoverSmooth")).Begin();
+		}
+
+		private Player _player;
+
+		private void ButtonNext_Click(object sender, RoutedEventArgs e)
+		{
+			(App.Current.MainWindow as DoubanFMWindow).Next();
+		}
+
+		private void ButtonNever_Click(object sender, RoutedEventArgs e)
+		{
+			(App.Current.MainWindow as DoubanFMWindow).Never();
+		}
+
+		private void ShareButton_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// 在此处添加事件处理程序实现。
+			if (_player.CurrentSong != null)
+				new Share(_player, (Share.Sites)((FrameworkElement)e.Source).Tag).Go();
+		}
+
+		private void CheckBoxShowLyrics_Checked(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// 在此处添加事件处理程序实现。
+			if (_player != null) (App.Current.MainWindow as DoubanFMWindow).ShowLyrics();
+		}
+
+		private void CheckBoxShowLyrics_Unchecked(object sender, RoutedEventArgs e)
+		{
+			if (_player != null) (App.Current.MainWindow as DoubanFMWindow).HideLyrics();
+		}
+
+		private void BtnCopyUrl_Click(object sender, RoutedEventArgs e)
+		{
+			if (_player.CurrentSong != null)
+			{
+				new Share(_player).Go();
+				MessageBox.Show(App.Current.MainWindow, "地址已复制到剪贴板", "复制成功", MessageBoxButton.OK, MessageBoxImage.Information);
+			}
+		}
+
+		private void BtnOneKeyShare_Click(object sender, RoutedEventArgs e)
+		{
+			(App.Current.MainWindow as DoubanFMWindow).OneKeyShare();
+		}
+	}
+}
---
+++ README.txt
@@ -0,0 +1,7 @@
+本软件为豆瓣FM（http://douban.fm）的第三方客户端，服务器归豆瓣所有，非本软件作者。本软件中所有来自网络的音乐与专辑封面的版权归豆瓣及相应的音乐公司所有。
+
+本软件的通知区域图标使用了Philipp Sumi的hardcodet.net NotifyIcon for WPF第三方库（http://www.hardcodet.net/projects/wpf-notifyicon），并对其做了小幅修改。
+
+Copyright (C) 2011 K.F.Storm
+
+软件主页：http://www.kfstorm.com/blog/doubanfm
