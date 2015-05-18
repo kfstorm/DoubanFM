c4b3940e5e45811023c63323b6defaa64c29f015
Kai Yang <yk000123@sina.com>
2015-05-18 13:39:01 +0800

    Add build.cmd

---
+++ build.cmd
@@ -0,0 +1,32 @@
+@echo off
+if not defined VisualStudioVersion (
+    if defined VS140COMNTOOLS (
+        call "%VS140COMNTOOLS%\VsDevCmd.bat"
+        goto :Build
+    )
+
+    if defined VS120COMNTOOLS (
+        call "%VS120COMNTOOLS%\VsDevCmd.bat"
+        goto :Build
+    )
+
+    if defined VS110COMNTOOLS (
+        call "%VS110COMNTOOLS%\VsDevCmd.bat"
+        goto :Build
+    )
+
+    if defined VS100COMNTOOLS (
+        call "%VS110COMNTOOLS%\VsDevCmd.bat"
+        goto :Build
+    )
+
+    echo Error: build.cmd requires Visual Studio 2010, 2012, 2013 or 2015.  
+    exit /b 1
+)
+
+:Build
+MSBuild DoubanFM.sln /t:Rebuild /p:Configuration=Release
+
+:Copy
+rd bin /s /q
+robocopy DoubanFM\bin\Release bin *.dll *.exe *.config version.dat /MIR /XD
