abf0f25ea981a7c4db9a3cceae1aa4be394938e9
Kai Yang <yk000123@sina.com>
2015-05-29 13:37:14 +0800

    Separate installer generator to another batch file

--- build.cmd
+++ build.cmd
@@ -31,18 +31,3 @@ if %errorlevel% NEQ 0 exit /b 1
 :Copy
 rd bin /s /q
 robocopy DoubanFM\bin\Release bin *.dll *.exe *.config version.dat /MIR /XD app.publish
-
-:CreateInstaller
-if exist "%ProgramFiles(x86)%\NSIS\Unicode\makensis.exe" (
-    if exist "%ProgramFiles(x86)%\NSIS\Unicode\Plugins\FindProcDLL.dll" (
-        for /F %%I IN (bin\version.dat) DO (
-            pushd ScriptForInstaller
-            make.bat %%I
-            popd
-        )
-        goto :EOF
-    )
-)
-
-echo NSIS is not found or not ready. Will not create the installer.
-exit /b 1
